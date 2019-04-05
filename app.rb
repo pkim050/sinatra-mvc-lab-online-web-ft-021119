0c536f96c359f6488f3e0ced2de18ad8e865198b
Patrick Kim <pkim050@gmail.com>
2019-04-05 04:11:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinizer' do
+  post '/user_input' do
     @pig_latin = PigLatinizer.new(params[:string])
     erb :pig_latin
   end
