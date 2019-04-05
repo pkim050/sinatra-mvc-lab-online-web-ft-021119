28a82f5bc98d2074eb66e307530b756ced41d2de
Patrick Kim <pkim050@gmail.com>
2019-04-05 03:15:12 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,31 @@
 class PigLatinizer
+  attr_accessor :string
 
+  def initialize(string)
+    @string = string
+  end
+
+  def convert
+    a = @string.split(" ")
+    final = ""
+    b = a.map {|element| convert2(element)}
+    final = b.join(" ")
+  end
+
+  def convert2(word)
+    first_letter = word[0].downcase
+    counter = word.size
+    counter2 = 1
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+      "#{word}way"
+    else
+      temp = pop()
+      word.push(temp)
+      while counter2 < counter
+        break if ["a", "e", "i", "o", "u"].include?(word[counter2])
+        temp = pop()
+        word.push(temp)
+        counter2 += 1
+      end
+    end
 end
